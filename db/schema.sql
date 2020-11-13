4c6f49aca7bf9a79e8b17be032057ad77679ffe4
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-11-13 05:33:14 -0700

    Added schema and seeds file to the db folder

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,75 @@
-DROP DATABASE IF EXISTS project_2_local;
-CREATE DATABASE project_2_local;
+DROP DATABASE IF EXISTS timesTables_db;
+CREATE DATABASE timesTables_db;
+USE timesTables_db;
 
-DROP DATABASE IF EXISTS project_2_test;
-CREATE DATABASE project_2_test;
+-- Still need to create TABLE for username/results/etc
+
+CREATE TABLE one_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE two_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE three_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE four_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE five_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE six_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE seven_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE eight_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE nine_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE ten_times (
+	id INT AUTO_INCREMENT NOT NULL,
+    equations varchar(255) NOT NULL,
+    answers varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
